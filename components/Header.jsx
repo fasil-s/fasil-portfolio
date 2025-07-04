526e3f86174084edf93384be651b473953bd3201
fasil <fasilsebsibe27@gmail.com>
2025-07-04 16:03:01 +0300

    add the header and navbar setup

---
+++ components/Header.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import Navbar from "./Navbar";
+
+export default function Header() {
+  return (
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link href="/">
+          <h1 className="text-4xl font-semibold">
+            Fasil<span className="text-accent">.</span>
+          </h1>
+        </Link>
+        {/* destop navigation */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Navbar />
+          <Link href="/contact">
+            <Button>Hire me</Button>
+          </Link>
+        </div>
+        {/* mobile navigation */}
+        <div className="xl:hidden">mobile navbar</div>
+      </div>
+    </header>
+  );
+}
